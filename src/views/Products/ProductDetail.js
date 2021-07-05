b9f2374e53a1135119d2e1b3bfc43f29a46b8bf1
mcd_oblivion <mcd.oblivion@gmail.com>
2021-07-05 10:24:49 +0700

    Comment feature

--- src/views/Products/ProductDetail.js
+++ src/views/Products/ProductDetail.js
@@ -17,6 +17,13 @@ import Parallax from "components/Parallax/Parallax.js";
 import Card from "components/Card/Card.js";
 import Carousel from "react-slick";
 
+import Slide from "@material-ui/core/Slide";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import IconButton from "@material-ui/core/IconButton";
+
 import styles from "assets/jss/material-kit-react/views/profilePage.js";
 
 import CardBody from "components/Card/CardBody";
@@ -28,24 +35,47 @@ import {
   Check,
   CommentRounded,
   RemoveRounded,
+  Close,
 } from "@material-ui/icons";
 import { Input, InputAdornment } from "@material-ui/core";
 import SnackbarContent from "components/Snackbar/SnackbarContent";
 
 const useStyles = makeStyles(styles);
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
+
 export default function ProductDetail(props) {
   const { ...rest } = props;
   const productId = rest.location.state.productId;
 
+  const [userId, setUserId] = useState("");
   const [product, setProduct] = useState({});
   const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState({ rating: 5, comment: "" });
+  const [ownComment, setOwnComment] = useState({
+    _id: "",
+    rating: 5,
+    comment: "",
+  });
   const [quantity, setQuantity] = useState("1");
+  const [showModalUpdateComment, setShowModalUpdateComment] = useState(false);
   const [alert, setAlert] = useState({
     show: false,
     msg: "",
     success: false,
   });
+
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get("/users/jwt-info");
+      console.log(response.data);
+      setUserId(response.data.user._id);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
 
   const getProduct = async () => {
     try {
@@ -70,6 +100,24 @@ export default function ProductDetail(props) {
         const comments = response.data.data;
         setComments(comments);
       }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getOwnComment = async (commentId) => {
+    try {
+      const response = await axiosInstance.get(
+        `/products/comments/${commentId}`
+      );
+      const foundComment = response.data.data;
+      console.log("Own comment:", foundComment);
+      if (foundComment)
+        setOwnComment({
+          _id: foundComment._id,
+          comment: foundComment.comment,
+          rating: foundComment.rating,
+        });
     } catch (error) {
       console.log(error);
     }
@@ -143,6 +191,54 @@ export default function ProductDetail(props) {
     }
   };
 
+  const createComment = async () => {
+    try {
+      const response = await axiosInstance.post(
+        `/products/${productId}/comments`,
+        { ...newComment }
+      );
+      getComments();
+      getProduct();
+
+      console.log(response.data);
+
+      setAlert({
+        show: true,
+        success: true,
+        msg: response.data.msg,
+      });
+
+      setTimeout(() => {
+        setAlert({ ...alert, show: false });
+      }, 3000);
+    } catch (error) {
+      console.log(error.response);
+      setAlert({
+        show: true,
+        success: false,
+        msg: error.response.data.msg,
+      });
+
+      setTimeout(() => {
+        setAlert({ ...alert, show: false });
+      }, 3000);
+    }
+  };
+
+  const updateComment = async (commentId) => {
+    try {
+      const response = await axiosInstance.put(
+        `/products/${productId}/comments/${commentId}`,
+        { ...ownComment }
+      );
+      console.log(response.data);
+      getComments();
+      getProduct();
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
   const changeQuantity = (e, value) => {
     if (value === 1 || value === -1) {
       if (Number(quantity) + value > 0)
@@ -162,7 +258,47 @@ export default function ProductDetail(props) {
     }
   };
 
+  const handleNewComment = (e) => {
+    setNewComment({ ...newComment, comment: e.target.value });
+    if (e.keyCode === 13 && newComment.comment) {
+      createComment();
+      setNewComment({ rating: 5, comment: "" });
+    }
+  };
+
+  const handleOwnComment = (e) => {
+    setOwnComment({ ...ownComment, comment: e.target.value });
+    if (e.keyCode === 13 && ownComment.comment) {
+      updateComment(ownComment._id);
+      setOwnComment({ id: "", rating: 5, comment: "" });
+      getComments();
+      setShowModalUpdateComment(false);
+    }
+  };
+
+  const handleDeleteComment = (commentId) => async () => {
+    try {
+      const response = await axiosInstance.delete(
+        `/products/${productId}/comments/${commentId}`
+      );
+      console.log(response.data);
+      getComments();
+    } catch (error) {
+      console.log(error.response);
+      setAlert({
+        show: true,
+        success: false,
+        msg: error.response.data.msg,
+      });
+
+      setTimeout(() => {
+        setAlert({ ...alert, show: false });
+      }, 3000);
+    }
+  };
+
   useEffect(() => {
+    getUserInfo();
     getProduct();
     getComments();
   }, []);
@@ -194,6 +330,7 @@ export default function ProductDetail(props) {
         filter
         image={require("assets/img/bg-home.png").default}
       />
+
       <div className={classNames(classes.main, classes.mainRaised)}>
         <div className="product">
           <div className={classes.container}>
@@ -315,20 +452,157 @@ export default function ProductDetail(props) {
                   comments.map((pComment) => {
                     const { _id, author, rating, comment } = pComment;
                     return (
-                      <Card key={_id} style={{ padding: "0 1rem" }}>
-                        <h4>{author.firstName + " " + author.lastName}</h4>
-                        <Rating value={rating} readOnly></Rating>
-                        <p>{comment}</p>
+                      <Card key={_id}>
+                        <div
+                          style={{
+                            padding: "0 1rem",
+                            display: "flex",
+                            flexWrap: "wrap",
+                            justifyContent: "space-between",
+                          }}
+                        >
+                          <div>
+                            <h4>{author.firstName + " " + author.lastName}</h4>
+                            <Rating value={rating} readOnly></Rating>
+                            <p>{comment}</p>
+                          </div>
+                          {userId === author._id && (
+                            <div>
+                              <Dialog
+                                classes={{
+                                  root: classes.center,
+                                  paper: classes.modal,
+                                }}
+                                open={showModalUpdateComment}
+                                TransitionComponent={Transition}
+                                keepMounted
+                                onClose={() => setShowModalUpdateComment(false)}
+                                aria-labelledby="modal-slide-title"
+                                aria-describedby="modal-slide-description"
+                              >
+                                <DialogTitle
+                                  id="classic-modal-slide-title"
+                                  disableTypography
+                                  className={classes.modalHeader}
+                                  style={{
+                                    display: "flex",
+                                    justifyContent: "space-between",
+                                  }}
+                                >
+                                  <h4 style={{ fontWeight: "bold" }}>
+                                    Edit comment
+                                  </h4>
+                                  <IconButton
+                                    style={{
+                                      position: "absolute",
+                                      top: "0",
+                                      right: "0",
+                                    }}
+                                    key="close"
+                                    aria-label="Close"
+                                    color="inherit"
+                                    onClick={() =>
+                                      setShowModalUpdateComment(false)
+                                    }
+                                  >
+                                    <Close className={classes.modalClose} />
+                                  </IconButton>
+                                </DialogTitle>
+                                <DialogContent
+                                  id="modal-slide-description"
+                                  className={classes.modalBody}
+                                >
+                                  <Card style={{ padding: "1rem" }}>
+                                    <Rating
+                                      onChange={(e, updateRating) =>
+                                        setOwnComment({
+                                          ...ownComment,
+                                          rating: updateRating,
+                                        })
+                                      }
+                                      name="update-rating"
+                                      value={ownComment.rating}
+                                    ></Rating>
+                                    <CustomInput
+                                      id="update-comment"
+                                      formControlProps={{
+                                        fullWidth: true,
+                                      }}
+                                      inputProps={{
+                                        endAdornment: (
+                                          <InputAdornment position="end">
+                                            <CommentRounded />
+                                          </InputAdornment>
+                                        ),
+                                        value: ownComment.comment,
+                                        onChange: handleOwnComment,
+                                        onKeyUp: handleOwnComment,
+                                      }}
+                                    />
+                                  </Card>
+                                </DialogContent>
+                                <DialogActions
+                                  className={
+                                    classes.modalFooter +
+                                    " " +
+                                    classes.modalFooterCenter
+                                  }
+                                >
+                                  <Button
+                                    onClick={() =>
+                                      setShowModalUpdateComment(false)
+                                    }
+                                  >
+                                    Cancel
+                                  </Button>
+                                  <Button
+                                    onClick={() => {
+                                      setShowModalUpdateComment(false);
+                                      updateComment(ownComment._id);
+                                    }}
+                                    color="success"
+                                  >
+                                    Update comment
+                                  </Button>
+                                </DialogActions>
+                              </Dialog>
+                              <Button
+                                onClick={() => {
+                                  getOwnComment(_id);
+                                  setShowModalUpdateComment(true);
+                                }}
+                                style={{ margin: "0" }}
+                                color="transparent"
+                              >
+                                Edit
+                              </Button>
+                              <br />
+                              <Button
+                                onClick={handleDeleteComment(_id)}
+                                style={{ margin: "0" }}
+                                color="transparent"
+                              >
+                                Delete
+                              </Button>
+                            </div>
+                          )}
+                        </div>
                       </Card>
                     );
                   })}
               </GridItem>
               <GridItem xs={12} sm={8}>
                 <Card style={{ padding: "1rem" }}>
-                  <Rating name="final-rating" defaultValue={5}></Rating>
+                  <Rating
+                    onChange={(e, newRating) =>
+                      setNewComment({ ...newComment, rating: newRating })
+                    }
+                    name="new-rating"
+                    value={newComment.rating}
+                  ></Rating>
                   <CustomInput
                     labelText="Write comment"
-                    id="material"
+                    id="new-comment"
                     formControlProps={{
                       fullWidth: true,
                     }}
@@ -338,6 +612,9 @@ export default function ProductDetail(props) {
                           <CommentRounded />
                         </InputAdornment>
                       ),
+                      value: newComment.comment,
+                      onChange: handleNewComment,
+                      onKeyUp: handleNewComment,
                     }}
                   />
                 </Card>
